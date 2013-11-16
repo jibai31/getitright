45948590d5ad51a87934e93527dce74e0cccaa04
Jean-Baptiste Goulain <jibai31@gmail.com>
2013-11-16 14:19:34 +0000

    Init.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Getitright::Application.routes.draw do
+  devise_for :users,
+    controllers: {
+      registrations: "registrations",
+      omniauth_callbacks: "authentications"
+    }
+
   root 'static_pages#home'
+
 end
